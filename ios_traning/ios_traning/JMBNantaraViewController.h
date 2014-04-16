128f35befe2e1eed0c3a9790a33f2c2b4ccce1eb
jumbo ken <subsubmailer@gmail.com>
2014-04-17 06:48:09 +0900

    サンプルの画面、objC文法追加

---
+++ ios_traning/ios_traning/JMBNantaraViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMBNantaraViewController.h
+//  ios_traning
+//
+//  Created by 羽田 健太郎 on 2014/04/16.
+//  Copyright (c) 2014年 羽田 健太郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMBNantaraViewController : UIViewController
+
+@end
---
+++ ios_traning/ios_traning/JMBNantaraViewController.m
@@ -0,0 +1,103 @@
+//
+//  JMBNantaraViewController.m
+//  ios_traning
+//
+//  Created by 羽田 健太郎 on 2014/04/16.
+//  Copyright (c) 2014年 羽田 健太郎. All rights reserved.
+//
+
+#import "JMBNantaraViewController.h"
+
+@interface JMBNantaraViewController ()
+
+@end
+
+@implementation JMBNantaraViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.view setBackgroundColor:[UIColor redColor]];
+    
+    // 数字
+    NSInteger a = 10;
+    int b = 10;
+    
+    // 文字列
+    NSString* str = @"moji";
+    
+    // 配列
+    // 固定長配列
+    NSArray* arr = @[@"1", @"2", @"もじ"];
+    // 可変長配列
+    // [インスタンス メソッド];
+    // おいしい = [たいやき　食べる:2 :10];
+    NSMutableArray* arrM = [[NSMutableArray alloc] initWithArray:arr];
+
+    // 関数
+    UIColor* redParama = [UIColor redColor];
+    [self setBackColorDaze:redParama :str];
+    
+    // for文 - ループ
+    for (id s in arr)
+    {
+        NSLog(@"%@ - %@ - %d", (NSString*)s, @"str", 10);
+    }
+    
+    // for文
+    // whileとかある
+    // int型のiっていうカウンターの設定; for文が回る設定; for文が一回まわるたびにふえる数
+    for (int i = 0;;i++)
+    {
+        NSLog(@"%@ - %@ - %d", arr[i], @"str", 10);
+    }
+    
+    // if
+    if([str isEqual: @"moji"])
+    {
+        NSLog(@"if bun");
+    
+    }else
+    {
+        NSLog(@"else");
+    }
+    
+    // switchもある
+    // 引数（ここでいうbはひきすう)
+    switch (b) {
+        case 10:
+            NSLog(@"10");
+            break;
+            
+        default:
+            break;
+    }
+
+    
+}
+
+// 画面色を変えるメソッド
+- (void)setBackColorDaze:(UIColor*)color :(NSString*)str
+{
+    self.view.backgroundColor = color;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
